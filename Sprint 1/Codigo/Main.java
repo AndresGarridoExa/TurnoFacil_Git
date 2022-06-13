c632b75d1829f43657760e75e32efab73fee9626
hilenv <104539078+hilenv@users.noreply.github.com>
2022-06-13 19:11:39 -0300

    Update Main.java

--- Sprint 1/Codigo/Main.java
+++ Sprint 1/Codigo/Main.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 public class Main {
     public static void main( String[] args) {
-        Paciente p = new Paciente(123,"pepito", "a", "a", 123, "a", "IOMA", 123);
-        Medico m1 = new Medico(111, "luis", "a", "Guardia1","IOMA", "Lunes", "5");
-        Medico m2 = new Medico(112, "juan", "a", "Guardia1","IOMA", "Lunes", "5");
+        Paciente p = new Paciente(40568956,"Marcelo", "Diaz", "San Martin 123", 12345678, "a", "IOMA", 14589);
+        Medico m1 = new Medico(20456897, "Luis", "Alvarez", "Guardia","IOMA", "Lunes", "17");
+        Medico m2 = new Medico(32458795, "Juan", "Lopez", "Cardiologo","OSDE", "Viernes", "16");
+        Medico m3 = new Medico(32458795, "Daniela", "Morales", "Ginecologo","Osprera", "Lunes", "18");
+        Medico m4 = new Medico(32458795, "Pablo", "Diaz", "Dentista","Medife", "Mastes", "16");
+        Medico m5 = new Medico(32458795, "Victoria", "Gonzalez", "Pediatra,","IOMA", "Jueves", "13");
+        Medico m6 = new Medico(32458795, "Lucia", "Lopez", "Guardia","OSDE", "Miercoles", "12");
         ArrayList<Medico> lista = new ArrayList<Medico>();
         lista.add(m1);
         lista.add(m2);
