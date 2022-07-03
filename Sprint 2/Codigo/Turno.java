b841ebf694cbb71109bbedc09f333e8938253ae1
Fran Zárate <franpilarz@gmail.com>
2022-07-03 09:26:07 -0300

    Update Turno.java

--- Sprint 2/Codigo/Turno.java
+++ Sprint 2/Codigo/Turno.java
@@ -60,29 +60,31 @@ public class Turno {
         for (int i = 0; i < verMedicos(c).size(); i++){
             System.out.println(i+1 + ") " + verMedicos(c).get(i).nombre + ", " + verMedicos(c).get(i).especialidad + ", " + verMedicos(c).get(i).obraSocial);
         }
-        try (Scanner in = new Scanner (System.in)) {
-			int s = in.nextInt();
-			Medico m = (verMedicos(c).get(s-1));
-			System.out.println("Elija un turno disponible para esta semana: ");
-			for (int i = 0; i < verMedicos(c).listaTurnos.size(); i++){
-            			System.out.println(i+1 + ") " + verMedicos(c).listaTurnos(i).toString() + " ");
-        		}
-			int t = in.nextInt();
-			try (Scanner in2 = new Scanner (System.in)) {
-				System.out.println("¿Desea confirmar el turno? s/n");
-				String s1 = in2.nextLine();
-				if (!s1.equals("n")){
-				    imprimirTurno(m,t);
-				    System.out.println("Si desea recibir el turno por correo electrónico, ingrese su dirección a continuación, de lo contrario ingrese "n");
-				    String s2 = in2.nextLine();
-				    System.out.println("Turno enviado, revise su bandeja de entrada o de spam si no puede encontrarlo");
-				}else{
-				    System.out.println("Turno no confirmado");
-				}
-			}
+            Scanner in = new Scanner (System.in);
+            int s = in.nextInt();
+            Medico m = (verMedicos(c).get(s-1));
+            System.out.println("Elija un turno disponible para esta semana: ");
+            for (int i = 0; i < m.listaTurnos.size(); i++){
+                System.out.println(i+1 + ") " + m.listaTurnos.get(i) + " ");
+            }
+            int t = in.nextInt();
+            Scanner in2 = new Scanner (System.in);
+                System.out.println("¿Desea confirmar el turno? s/n");
+                String s1 = in2.nextLine();
+                if (!s1.equals("n")){
+                    imprimirTurno(m,t);
+                    System.out.println("Si desea recibir el turno por correo electrónico," +
+                            "ingrese su dirección a continuación, de lo contrario ingrese n ");
+                    String s2 = in2.nextLine();
+                    if (!s2.equals("n")) {
+                        System.out.println("Turno enviado, revise su bandeja de entrada o de spam si no puede encontrarlo");
+                    }
+                }else{
+                    System.out.println("Turno no confirmado");
+                }
+        }
+        public void imprimirTurno (Medico m, int t){
+            System.out.println("Usted tiene turno el: " + m.listaTurnos.get(t));
+            System.out.println("Con el medico:  " + m.nombre + ", dni del medico: " + m.DNI);
+        }
     }
-    public void imprimirTurno(Medico m, int t){
-	    System.out.println("Usted tiene turno el: " + m.listaTurnos(t));
-	    System.out.println("Con el medico:  " + m.nombre() + ", dni del medico: " + m.dni);
-    }
-}
